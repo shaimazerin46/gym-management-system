a7f39e1682c4e7b24f6cbe981e57d4b4f33be7a5
unknown <shaimazerin46@gmail.com>
2025-05-28 20:24:09 +0600

    login page design done

---
+++ src/app/login/page.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function loginPage() {
+  return (
+    <div>
+        <h3>Login here</h3>
+        <div className="card bg-base-100 w-[500px] mx-auto  shrink-0 shadow-2xl ">
+      <div className="card-body">
+        <form className="fieldset">
+          <label className="label">Email</label>
+          <input type="email" className="input w-full" placeholder="Email" />
+          <label className="label">Password</label>
+          <input type="password" className="input w-full" placeholder="Password" />
+          <div>
+            <p>{`Don't`} have an account?<>signup</></p>
+
+          </div>
+          <button className="btn btn-primary mt-4">Login</button>
+        </form>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default loginPage
---
+++ src/app/register/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function registerPage() {
+  return (
+    <div>page</div>
+  )
+}
+
+export default registerPage
